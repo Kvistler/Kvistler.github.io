beb58d424b15374776766c7cdc49e69d9fdf34e1
Kvistler <Kvistler@users.noreply.github.com>
2019-10-25 16:25:28 +0300

    update

--- public/code_lab.js
+++ public/code_lab.js
@@ -1,18 +1,18 @@
-function calculate() {
-    const checkboxes = document.querySelectorAll("[name='subject']");
-    let sum = 0;
-    for (let temp of checkboxes) {
+function calculate () {
+  const checkboxes = document.querySelectorAll("[name='subject']")
+  let sum = 0
+  for (let temp of checkboxes) {
+    if (temp.checked) {
+      sum += Number(temp.value)
+      temp = temp.nextElementSibling
+      for (let i = 0; i < 6; i++) {
         if (temp.checked) {
-            sum += Number(temp.value);
-            temp = temp.nextElementSibling;
-            for (let i = 0; i < 6; i++) {
-                if (temp.checked) {
-                    sum += Number(temp.value);
-                }
-                temp = temp.nextElementSibling;
-            }
+          sum += Number(temp.value)
         }
+        temp = temp.nextElementSibling
+      }
     }
-    document.querySelector("[name='price']").value = sum + '$';
+  }
+  document.querySelector("[name='price']").value = sum + '$'
 }
-document.querySelector('#someaction').addEventListener('click', calculate);
+document.querySelector('#someaction').addEventListener('click', calculate)
